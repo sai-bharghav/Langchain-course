0b719bbc624fb9480dd9fe5cd4b321d1d8d4f2cb
Sai Bharghav Chollangi <chollangi7us@gmail.com>
2026-02-21 11:37:47 -0600

    Chains file added

---
+++ chains7.py
@@ -0,0 +1,25 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system", "You are a viral twitter influencer grading a tweet, generate critique and recommendations for the user's tweet. Always provide a detailed recommendations, including requests for length, virality, style, etc"
+        ), MessagesPlaceholder(variable_name="messages")
+    ]
+)
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a twitter tehie influencer assistant tasked with writing excellent twitter posts."
+            "Generate the best twitter post possible for the user's request."
+            "If the user provides critique, respond with a revised version of your previous attempts."
+        ), MessagesPlaceholder(variable_name="messages")    
+    ]  
+)
+
+llm = ChatOpenAI(temperature=0.5, model="gpt-4o")
+generation_chain = generation_prompt | llm
+reflection_chain = reflection_prompt | llm
